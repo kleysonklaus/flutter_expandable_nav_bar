78b4cc30d31b61f72bc0ee042a112eb6a8c592f0
kleyson klaus <kleyson.zur44@gmail.com>
2022-11-15 22:39:07 -0500

    feat(music): data music

---
+++ lib/app/music.dart
@@ -0,0 +1,15 @@
+class Music {
+  final String singer;
+  final String name;
+  final String backGroundImage;
+  final String singerImage;
+
+  Music({
+    required this.singer,
+    required this.name,
+    required this.backGroundImage,
+    required this.singerImage,
+  });
+}
+
+final _assetPath = 'assets/expandable_nav_bar';
